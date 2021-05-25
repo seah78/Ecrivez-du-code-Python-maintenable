cee3a16369f474f7274b9b84146dbb20eaf32f64
seah78 <githubseah.pro>
2021-05-25 18:09:45 +0200

    models

---
+++ main.py
@@ -0,0 +1,22 @@
+
+"""Entry point."""
+
+from models.card import Card
+from models.deck import Deck
+from models.player import Player
+
+
+if __name__ == "__main__":
+    card1 = Card("trefles", "cinq")
+    card2 = Card("coeurs", "cinq")
+    card3 = Card("coeurs", "valet")
+    print(card1 < card2)
+    print(card3 > card2)
+
+    deck = Deck()
+    print(deck)
+
+
+    player = Player("John")
+    player.hand.append(deck.draw_card())
+    print(player.hand)
